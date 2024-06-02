cb031e29715011c5be57432d4b11d6ed5f840f16
хлифи <70767436+xllifi@users.noreply.github.com>
2024-06-02 15:51:01 +1000

    Update mcrcsc.user.js

--- mcrcsc.user.js
+++ mcrcsc.user.js
@@ -7,14 +7,28 @@
 // @match        https://team.minecraft.rent/*
 // @icon         https://minecraft.rent/favicon.ico
 // @grant        GM_addStyle
-// @grant        GM_getResourceText
-// @resource     IMPORT_MCRCCSS https://raw.githubusercontent.com/xllifi/files/main/mcrccss.css
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    console.log("Патчи применяются!");
-    const mcrccss = GM_getResourceText("IMPORT_MCRCCSS");
-    GM_addStyle(mcrccss);
-    console.log("CSS применён!");
+(function () {
+       'use strict';
+       console.log("Скрипт с патчами начал работу.");
+       /* Находим панель со вкладками */ const tabsBar = document.getElementsByClassName("w-full h-[60px] bg-[#22293b] rounded-lg flex items-center gap-6 px-6")[0];
+       /* Создаём элемент со статусом */ const statusMsg = document.createElement('a'); statusMsg.className = 'text-white'; statusMsg.style.cssText = 'margin-left: auto; margin-right: 0.5rem; padding: 0.25rem 0.5rem; border-radius: 0.25rem;';
+
+       // Запрашиваем стиль с гитхаба
+       var req = new XMLHttpRequest();
+       req.open('GET', 'https://raw.githubusercontent.com/xllifi/files/main/mcrccss.css', false); req.send();
+       // Если стиль до нас дошёл, применяем
+       if (req.status == 200) {
+              GM_addStyle(req.responseText);
+              console.log("[Патчи] CSS загружен с GitHub и применён");
+              statusMsg.style.setProperty('background-color', 'rgba(34, 197, 94, 0.5)')
+              statusMsg.textContent = 'CSS патча загружен';
+              tabsBar.appendChild(statusMsg);
+       } else {
+              console.error("[Патчи] Не удалось загрузить CSS с GitHub");
+              statusMsg.style.setProperty('background-color', 'rgba(239, 68, 68, 0.5)')
+              statusMsg.textContent = 'Не удалось получить CSS патча!';
+              tabsBar.appendChild(statusMsg);
+       }
 })();
