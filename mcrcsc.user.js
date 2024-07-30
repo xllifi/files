b65365a261d5a6398c05750205f2999305c59d51
хлифи <70767436+xllifi@users.noreply.github.com>
2024-07-30 22:27:50 +1000

    0.0.25
    - Добавлена функция авто-прокрутки консоли.

--- mcrcsc.user.js
+++ mcrcsc.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Патчи админки Minecraft.RENT
 // @namespace    https://xllifi.ru
-// @version      0.0.24
+// @version      0.0.25
 // @description  Улучшения для админ-панели Minecraft.RENT
 // @author       xllifi
 // @match        https://*.minerent.net/*
@@ -13,8 +13,12 @@
 // ==/UserScript==
 
 'use strict';
-var expandIcon = `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='m21 21-6-6m6 6v-4.8m0 4.8h-4.8'/><path d='M3 16.2V21m0 0h4.8M3 21l6-6'/><path d='M21 7.8V3m0 0h-4.8M21 3l-6 6'/><path d='M3 7.8V3m0 0h4.8M3 3l6 6'/></svg>")`;
-var shrinkIcon = `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='m15 15 6 6m-6-6v4.8m0-4.8h4.8'/><path d='M9 19.8V15m0 0H4.2M9 15l-6 6'/><path d='M15 4.2V9m0 0h4.8M15 9l6-6'/><path d='M9 4.2V9m0 0H4.2M9 9 3 3'/></svg>")`;
+var expandIcon = `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='28' height='28' viewBox='0 0 28 28' fill='none' stroke-linecap='round' stroke-linejoin='round'><defs><g id='expandButtonPath'><path d='m21 23-6-6m6 6v-4.8m0 4.8h-4.8' /><path d='M3 18.2V23m0 0h4.8M3 23l6-6' /><path d='M21 9.8V5m0 0h-4.8M21 5l-6 6' /><path d='M3 9.8V5m0 0h4.8M3 5l6 6' /></g></defs><use href='%23expandButtonPath' stroke='%231c2230' stroke-width='8'></use><use href='%23expandButtonPath' stroke='white' stroke-width='2'></use></svg>")`;
+var shrinkIcon = `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='28' height='28' viewBox='0 0 28 28' fill='none' stroke-linecap='round' stroke-linejoin='round'><defs><g id='shrinkButtonPath'><path d='m15 17 6 6m-6-6v4.8m0-4.8h4.8' /><path d='M9 21.8V17m0 0H4.2M9 17l-6 6' /><path d='M15 6.2V11m0 0h4.8M15 11l6-6' /><path d='M9 6.2V11m0 0H4.2M9 11 3 5' /></g></defs><use href='%23shrinkButtonPath' stroke='%231c2230' stroke-width='8' /><use href='%23shrinkButtonPath' stroke='white' stroke-width='2' /></svg>")`;
+
+var scrollIconDisabled = `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='28' height='28' viewBox='0 0 28 28' fill='none' stroke-linecap='round' stroke-linejoin='round'><defs><g id='scrollButtonPath'><path d='M12 19V5'/><path d='m6 13 6 6 6-6'/><path d='M19 23H5'/></g></defs><use href='%23scrollButtonPath' stroke='%231c2230' stroke-width='8'/><use href='%23scrollButtonPath' stroke='white' stroke-width='2'/></svg>")`;
+var scrollIconEnabled  = `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='28' height='28' viewBox='0 0 28 28' fill='none' stroke-linecap='round' stroke-linejoin='round'><defs><g id='scrollButtonPath'><path d='M12 19V5'/><path d='m6 13 6 6 6-6'/><path d='M19 23H5'/></g></defs><use href='%23scrollButtonPath' stroke='%23147839' stroke-width='8'/><use href='%23scrollButtonPath' stroke='white' stroke-width='2'/></svg>")`;
+
 var tickIcon   = `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M18 6 7 17l-5-5'/><path d='m22 10-7.5 7.5L13 16'/></svg>")`;
 var updateIcon = `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M21 12a9 9 0 0 0-9-9 9.75 9.75 0 0 0-6.74 2.74L3 8'/><path d='M3 3v5h5'/><path d='M3 12a9 9 0 0 0 9 9 9.75 9.75 0 0 0 6.74-2.74L21 16'/><path d='M16 16h5v5'/></svg>")`;
 var dblQIcon   = `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M12.9,6.1c2-2.1,5.4-2.3,7.6-0.3c1.6,1.5,2.1,3.9,1.3,6.1c-1.6,3.3-7.2,2.6-7.2,2.6'/><path d='M13.2,19.6L13.2,19.6'/><path d='M2,9.6c0.1-2.1,2-3.7,4-3.5C7.7,6.2,9,7.3,9.5,8.9c0.6,2.4-2.6,4.7-2.6,4.7'/><path d='M8.2,18.3L8.2,18.3'/></svg>")`;
@@ -23,16 +27,18 @@ console.log("[Патчи] Скрипт начал работу.");
 
 /* ======== Переменные ======= */
 /*  Заполнитель высоты консоли */ const consoleHeightHolder = document.createElement('div'); consoleHeightHolder.style.cssText = 'height: 500px; width: 100%;'; consoleHeightHolder.id = 'consoleHeightHolder';
-/*   Элемент для переключателя */ const consoleExpandToggle = document.createElement('button'); consoleExpandToggle.id = 'consoleExpandToggle'; consoleExpandToggle.classList.add('consoleExpandToggle'); consoleExpandToggle.style.backgroundImage = expandIcon; consoleExpandToggle.setAttribute("onclick", "toggleConsoleExpand()");
+/*   Элемент для переключателя */ const consoleExpandToggle = document.createElement('button'); consoleExpandToggle.id = 'consoleExpandToggle'; consoleExpandToggle.classList.add('consoleButton'); consoleExpandToggle.classList.add('expand'); consoleExpandToggle.style.backgroundImage = expandIcon; consoleExpandToggle.setAttribute("onclick", "toggleConsoleExpand()");
+/*   Элемент для переключателя */ const consoleScrollToggle = document.createElement('button'); consoleScrollToggle.id = 'consoleScrollToggle'; consoleScrollToggle.classList.add('consoleButton'); consoleScrollToggle.classList.add('scroll'); consoleScrollToggle.style.backgroundImage = scrollIconDisabled; consoleScrollToggle.setAttribute("onclick", "toggleConsoleScroll()");
 /*    Враппер любимых серверов */ const favServerWrapper = document.createElement('div'); favServerWrapper.id = "favServerWrapper";
 /*  Текст для любимых серверов */ const favServerText = document.createElement('p'); favServerText.textContent = 'Избранные сервера:';
 /*     Кнопка любимого сервера */ const favServer = document.createElement('a');
 /*    Получаем массив серверов */ const favServersJSON = localStorage.getItem("favServersJSON");
 /*                   CSS стиль */ var cssElement = document.createElement('link'); cssElement.rel = 'stylesheet';
 /*                      Иконка */ var faviconLink = document.createElement('link'); faviconLink.rel = 'icon'; faviconLink.setAttribute('href', 'https://minecraft.rent/favicon.ico');
+/*      Проматывать ли консоль */ var scrollConsole = false;
 var favServersArray = [];
 if (favServersJSON != null) {
-    favServersArray = JSON.parse(favServersJSON); // Возвращает массив
+    favServersArray = JSON.parse(favServersJSON);
 }
 
 /* ========= Функции ========= */
@@ -69,7 +75,7 @@ function favClicked(actor) {
     updateFavServers();
 };
 function toggleConsoleExpand() {
-    const consoleWrapper = document.getElementById('console-scroll');
+    const consoleWrapper = document.querySelector('#console-scroll');
     var consoleWrapperClasses = consoleWrapper.className;
     if (consoleWrapperClasses.includes("expanded")) {
         consoleWrapper.classList.remove('expanded'); consoleWrapper.style.width = null; consoleWrapper.style.height = null; consoleWrapper.style.animation = 'consoleShrink 200ms';
@@ -85,6 +91,30 @@ function toggleConsoleExpand() {
         consoleWrapper.parentElement.insertBefore(consoleHeightHolder, consoleWrapper.nextElementSibling); consoleWrapper.style.animation = 'consoleExpand 200ms';
     }
 };
+function toggleConsoleScroll() {
+    const consoleWrapper = document.querySelector('#console');
+    if (consoleScrollToggle.style.backgroundImage == scrollIconDisabled) {
+        consoleScrollToggle.style.backgroundImage = scrollIconEnabled;
+        scrollConsole = true;
+    } else {
+        consoleScrollToggle.style.backgroundImage = scrollIconDisabled;
+        scrollConsole = false;
+    }
+
+    function scrollToBottom() {
+        if (scrollConsole) {
+            consoleWrapper.scroll({top: consoleWrapper.scrollHeight, left: consoleWrapper.scrollLeft, behavior: "smooth"});
+        };
+    }; 
+    scrollToBottom();
+
+    var observer = new MutationObserver(function (mutations) {
+        scrollToBottom();
+        console.log("[Патчи] Консоль промотана вниз");
+    });
+
+    observer.observe(consoleWrapper, {subtree: true, childList: true})
+};
 function randomString() {
     return Math.floor(Math.random() * Math.pow(10, 10));
 };
@@ -99,6 +129,9 @@ if (!unsafeWindow.favClicked) {
 }
 if (!unsafeWindow.toggleConsoleExpand) {
     unsafeWindow.toggleConsoleExpand = toggleConsoleExpand;
+}
+if (!unsafeWindow.toggleConsoleScroll) {
+    unsafeWindow.toggleConsoleScroll = toggleConsoleScroll;
 }
 if (!unsafeWindow.askAdminIP) {
     unsafeWindow.askAdminIP = askAdminIP;
@@ -178,6 +211,7 @@ document.addEventListener("DOMContentLoaded", async function () {
         if (window.location.href.match(/.+servers\/[\da-zA-Z]{8}/g) && !window.location.href.match(/.+servers\/[\da-zA-Z]{8}.+/g)) { // Главная страница
             /*            Находим элемент консоли */ const consoleWrapper = document.getElementById('console-scroll');
             consoleWrapper.appendChild(consoleExpandToggle);
+            consoleWrapper.appendChild(consoleScrollToggle);
         } else if (window.location.href.match(/.+servers\/[\da-zA-Z]{8}\/files\?dir.*/g)) { // Файлы сервера
             // Замена иконки
             const fileList = document.querySelector(".bg-\\[\\#22293b\\].p-4.rounded-lg").children
